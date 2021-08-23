a923cf354d1e2d6123aad6e494f1b3f7f6e99a3d
Chris Reynolds <chrisbyboston@gmail.com>
2021-08-22 21:35:23 -0400

    Starting install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew install stow gpg git-crypt paperkey qrencode zbar coreutils
+
+read -n1 -s -r -p $'Import your private gpg key then press any key to continue...\n' key
+
+mkdir -p $HOME/Documents/code/iwschris/
+cd $HOME/Documents/code/iwschris/
+
+git clone git@github.com:iwschris/DotFiles.git
+
+cd DotFiles || exit
+
+stow -t ~ .
